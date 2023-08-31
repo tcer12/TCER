3c66ef622f9648de996dfedfc68ee6903391f807
{ROA-DEV} <105471756+tcer12@users.noreply.github.com>
2023-08-31 17:23:46 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -115,3 +115,19 @@ preguntas.forEach((pregunta) => {
 
 	});
 });
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
