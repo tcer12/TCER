66194a1880e73b7f3bd6bd1859341b597ab1d0a2
{ROA-DEV} <105471756+tcer12@users.noreply.github.com>
2023-09-25 20:45:09 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,147 @@
+'use strict';
+
+
+
+/**
+ * add Event on elements
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+  if (elem.length > 1) {
+    for (let i = 0; i < elem.length; i++) {
+      elem[i].addEventListener(type, callback);
+    }
+  } else {
+    elem.addEventListener(type, callback);
+  }
+}
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+
+
+/**
+ * header & back top btn show when scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+const headerActive = function () {
+  if (window.scrollY > 80) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+  }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+const categorias = document.querySelectorAll('#categorias .categoria');
+const contenedorPreguntas = document.querySelectorAll('.contenedor-preguntas');
+let categoriaActiva = null;
+
+categorias.forEach((categoria) => {
+	categoria.addEventListener('click', (e) => {
+		categorias.forEach((elemento) => {
+			elemento.classList.remove('activa');
+		});
+
+		e.currentTarget.classList.toggle('activa');
+		categoriaActiva = categoria.dataset.categoria;
+
+
+		// Activamos el contenedor de preguntas que corresponde
+		contenedorPreguntas.forEach((contenedor) => {
+			if(contenedor.dataset.categoria === categoriaActiva){
+				contenedor.classList.add('activo');
+			} else {
+				contenedor.classList.remove('activo');
+			}
+		});
+	});
+});
+
+const preguntas = document.querySelectorAll('.preguntas .contenedor-pregunta');
+preguntas.forEach((pregunta) => {
+	pregunta.addEventListener('click', (e) => {
+		e.currentTarget.classList.toggle('activa');
+
+		const respuesta = pregunta.querySelector('.respuesta');
+		const alturaRealRespuesta = respuesta.scrollHeight;
+		
+		if(!respuesta.style.maxHeight){
+			// Si esta vacio el maxHeight entonces ponemos un valor.
+			respuesta.style.maxHeight = alturaRealRespuesta + 'px';
+		} else {
+			respuesta.style.maxHeight = null;
+		}
+
+		// [Opcional] Reiniciamos las demas preguntas
+		preguntas.forEach((elemento) => {
+			// Solamente queremos ejecutar el codigo para las preguntas que no 
+			// sean la pregunta a la que le dimos click.
+			if(pregunta !== elemento){
+				elemento.classList.remove('activa');
+				elemento.querySelector('.respuesta').style.maxHeight = null;
+			}
+		});
+
+
+	});
+});
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 4000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue +=1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+  }, duration);
+});
+
+const openModal = document.querySelector('.btn-secondary2');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal__close');
+
+openModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.add('modal--show');
+});
+
+closeModal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    modal.classList.remove('modal--show');
+});
